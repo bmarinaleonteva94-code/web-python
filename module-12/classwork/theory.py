0154d97b4ca18443242b301da81243dbd6f993da
bmarinaleonteva94-code <bmarina.leonteva.94@gmail.com>
2026-04-06 20:44:02 +0300

    cw: модуль-12

---
+++ module-12/classwork/theory.py
@@ -0,0 +1,458 @@
+# #  Singleton
+# class AppConfig:
+#     _instance = None
+#     def __new__(cls):
+#         if cls._instance is None:
+#             cls._instance = super().__new__(cls)
+#             cls._instance.mode = "prod"
+#         return cls._instance
+    
+
+# #  Factory Method
+# class EmailSender:
+#     def send(self, message):
+#         return f"Email: {message}"
+
+# class SmsSender:
+#     def send(self, message):
+#         return f"Sms: {message}"
+    
+# class NotificationFactory:
+#     @staticmethod
+#     def create(channel):
+#         if channel == "email":
+#             return EmailSender()
+#         if channel == "sms":
+#             return SmsSender()
+#         raise ValueError("Unknown channel")
+        
+# sender = NotificationFactory.create("sms")
+# sender.send("Ваш код подтверждения 1234")
+
+# #  Abstract Factory
+# class LightButton:
+#     def render(self):
+#         return "LightButton" 
+    
+# class LightInput:
+#     def render(self):
+#         return "LightInput"
+    
+# class DarkButton:
+#     def render(self):
+#         return "DarkButton"
+    
+# class DarkInput:
+#     def render(self):
+#         return "DarkInput"
+    
+# class LightThemeFactory:
+#     def create_button(self):
+#         return LightButton()
+    
+# class LightThemeFactory:
+#     def create_input(self):
+#         return LightInput()
+        
+# class DarkThemeFactory:
+#     def create_button(self):
+#         return DarkButton()
+    
+# class DarkThemeFactory:
+#     def create_input(self):
+#         return DarkInput()
+    
+# def build_form(factory):
+#     button = factory.create_button()
+#     field = factory.create_input()
+#     print(button.render(), field.render())
+
+# build_form(LightThemeFactory())
+
+# #   Builder
+# class LaptopBuilder:
+#     def __init__(self):
+#         self.laptop = {
+#             "cpu": "Intel i5",
+#             "ram": 8,
+#             "ssd": 256,
+#             "gru": "intergrated"
+#         }
+
+# def for_study(self):
+#     self.laptop["ram"] = 16
+#     self.laptop["ssd"] = 512
+#     return self
+
+# def for_gaming(self):
+#     self.laptop["ram"] = 32
+#     self.laptop["ssd"] = 1024
+#     self.laptop["gru"] = "RTX 4070"
+#     return self
+
+# def with_cpu(self, cpu):
+#     self.laptop["cpu"] = cpu
+#     return self
+
+# def build(self):
+#     return self.laptop.copy()
+
+# print(LaptopBuilder().for_study().with_cpu("Intel I7").build())
+
+
+# # Prototype
+# import copy
+# template_order = {
+#     "delivery": "standart",
+#     "promo": False,
+#     "items": ["book"]
+# }
+
+# fast_order = copy.deepcopy(template_order)
+# fast_order["delivery"] = "express"
+# print(template_order, fast_order)
+
+
+
+# #  Структурные паттерны
+# #   Adapter
+# class OldSmsService:
+#     def send_sms(self, phone, text):
+#         print(f"old service: {phone}: {text}")
+
+# class SmsAdapter:
+#     def __init__(self, service, phone):
+#         self.service = service
+#         self.phone = phone
+
+#         def send(self, message):
+#             self.service.send_sms(self.phone, message)
+
+# SmsAdapter(OldSmsService(), "+79999999999").send("Сообщение")
+
+# #  Bring
+# class TV:
+#     def turn_on(self):
+#         return "TV is on"
+# class Radio:
+#     def turn_on(self):
+#         return "Radio is on"
+
+# class Remove_Control:
+#     def __init__(self, device):
+#         self.device = device
+
+#     def power(self):
+#         return self.device.turn_on()
+# print(Remove_Control(TV()).power())
+
+# #  Composite
+# class File:
+#     def __init__(self, name, size):
+#         self.name = name
+#         self.size = size
+
+#     def get_size(self): 
+#         return self.size
+    
+# class Folder:
+#     def __init__(self, name):
+#         self.name = name
+#         self.children = []
+
+#     def add(self, child):
+#         self.children.append(child)
+
+#     def get_size(self):
+#         return sum(child.get_size() for child in self.children)
+    
+# docs = Folder("docs")
+# docs.add(File("text_1.txt", 10))
+# docs.add(File("text_2.txt", 20))
+# print(docs.get_size())
+
+
+# #  Decorator
+# class Coffee:
+#     def price(self):
+#         return 120
+    
+
+#     def description(self):
+#         return "Кофе"
+    
+# class MilkDecorator:
+#     def __init__(self, drink):
+#         self.drink = drink
+    
+#     def price(self):
+#         return self.drink.price() + 30
+    
+#     def description (self):
+#         return self.description() + ", молоко"
+    
+# class SyrupDecorator:
+#     def __init__(self, drink):
+#         self.drink = drink
+    
+#     def price(self): 
+#         return self.drink.price() + 25
+    
+#     def description (self):
+#         return self.drink.description() + ", сироп"
+    
+# drink = SyrupDecorator(MilkDecorator(Coffee()))
+# print(drink.price(), drink.description())
+
+
+# # Facade
+# class PaymentService:
+#     def pay(self, amount):
+#         print(f"Оплата {amount} подтверждена")
+
+# class WarehouseService:
+#     def reserve(self, item):
+#         print(f"Товар {item} зарезервирован")
+
+# class DeliveryService:
+#     def create(self, item):
+#         print(f" Доставка для {item} создана")
+
+# class OrderFacade:
+#     def __init__(self):
+#         self.payment = PaymentService()
+#         self.warehouse = WarehouseService()
+#         self.delivery = DeliveryService()
+
+#     def place_order(self, item, amount):
+#         self.payment.pay(amount)
+#         self.warehouse.reserve(item)
+#         self.delivery.create(item)
+#         print("Заказ оформлен")
+
+# OrderFacade().place_order("Наушники", 9999)
+
+
+# # Flyweight
+
+# class Flyweight:
+#     def __init__(self, color):
+#         self.color = color
+
+#     def draw(self, x, y):
+#         print(self.color, x, y)
+
+# class Factory:
+#     _cached = {}
+
+#     @classmethod
+#     def get(cls, color):
+#         if color not in cls._cached:
+#             cls._cached[color] = Flyweight(color)
+#         return cls._cached[color]
+    
+# red1 = Factory.get("red")
+# red2 = Factory.get("red")
+
+# print(red1 is red2)   # True
+
+
+# #  Proxy
+# class Image:
+#     def __init__(self, path):
+#         print("Загрузка")
+#         self.path = path
+
+#     def show (self):
+#         print(f"Show {self.path}")
+
+# class ImageProxy:
+#     def __init__(self, path):
+#         self.path = path
+#         self._real = None
+
+#     def show(self): 
+#         if self._real is None:
+#             self._real = Image(self.path)
+#         self._real.show()
+
+# img = Image("photo.png")
+# img.show()   #Загрузка
+# img.show()   # Ничего не выводит
+    
+
+
+# #  Поведенческие паттерны
+
+# #  Chain of Responsibility
+# class Handler:
+#     def __init__(self, next_handler=None):
+#         self.next_heandler = next_handler
+
+#     def handle(self, request):
+#         if self.next_heandler:
+#             return self.next_heandler.handle(request)
+#         return "Unhandled"
+
+# class AuthHandler(Handler):
+#     def handle(self, request):
+#         if not request.get("user"):
+#             return "401 Unathorized"
+#         return super().handle(request)
+    
+# class RoleHandler(Handler):
+#     def handle(self, request):
+#         if request.get("role") != "admin":
+#             return "403 Forbidden"
+#         return super().handle(request)
+    
+# chain = AuthHandler(RoleHandler)
+# print(chain.handle({"user": "Alice", "role": "admin"}))
+
+
+# # Command
+# class Light:
+#     def On(self):
+#         print("Light is on")
+
+# class TurnOnCommand:
+#     def __init__(self, light):
+#         self.light = light
+
+#     def execute(self):
+#         self.light.on()
+
+# class Button: 
+#     def __init__(self, command):
+#         self.command = command
+    
+#     def press(self):
+#         self.command.execute()
+
+# Button(TurnOnCommand(Light())).press()
+
+
+# # Mediator
+# class ChatMediator:
+#     def send(self, message, user):
+#         for c in user.colleagues:
+#             if c is not user:
+#                 c.receive(message)
+
+# class User:
+#     def __init__(self, name, mediator):
+#         self.name = name
+#         self.mediator = mediator
+#         self.collegues = []
+
+#     def send(self, message):
+#         self.mediator.send(f"{self.name}: {self.message}", self)
+
+#     def receive(self, message):
+#         print (message)
+
+# mediator = ChatMediator()
+# alice = User("Alice", mediator)
+# bob = User("Bob", mediator)
+# alice.collegues = [alice, bob]
+# bob.collegues = [alice, bob]
+# alice.send("hello")
+
+
+# # Memento
+# class Editor:
+#     def __init__(self):
+#         self.text = ""
+
+#     def write(self, text):
+#         self.text += text
+    
+#     def save(self):
+#         return self.text
+    
+#     def restore(self, snapshot):
+#         self.text = snapshot
+
+# editor = Editor()
+# editor.write("Hello")
+# snapshot = editor.save()
+# editor.write(", world")
+# print(editor.text)
+# editor.restore(snapshot)
+# print(editor.text)
+
+# # 
+# class Order:
+#     def __init__(self):
+#         self.subscribers = []
+
+#     def subscribe(self, listener):
+#         self.subscribers.append(listener)
+
+#     def set_status (self, status):
+#         for subscriber in self.subscribers:
+#             subscriber(status)
+
+# def email_listener(status):
+#     print(f"Email: произошла смена статуса {status}")
+
+# def sms_listener(status):
+#     print(f"SMS: произошла смена статуса {status}")
+
+# order = Order()
+# order.subscribe(email_listener)
+# order.subscribe(sms_listener)
+# order.set_status("delivery")
+
+
+#  State
+class DraftState:
+    def publish(self, document):
+        document.state = ReviewState()
+        return "Черновик отправлен на проверку"
+    
+class ReviewState:
+    def publish(self, document):
+        document.state = PublishedState()
+        return "Документ опубликован"
+    
+class PublishedState:
+    def publish(self, document):
+        return " Уже опубликовано"
+    
+class Document:
+    def __init__(self):
+        self.state = DraftState()
+
+    def publish(self):
+        return self.state.publish(self)
+    
+doc = Document()
+doc.publish()
+doc.publish()
+doc.publish()
+
+
+# Stategy
+class StandartDelivery:
+    def calculate(self, weight):
+        return 200 + weight * 10
+    
+class ExpressDelivery:
+    def calculate(self, weight):
+        return 500 + weight * 20
+    
+class PickupDelivery:
+    def calculate(self, weight):
+        return 0
+    
+class DeliveryCalc:
+    def __init__(self, stategy):
+        self.stategy = stategy
+
+    def get_price(self, weight):
+        return self.stategy.calculate(weight)
+    
+print(DeliveryCalc(PickupDelivery()).get_price(10))
+
+    
